09d51278fd0ca0a84661d3b81d5a8b6452de3ad0
yjpark1 <dmqm.yjpark@gmail.com>
2018-11-26 23:35:42 +0900

    update

--- models/generator.py
+++ models/generator.py
@@ -30,25 +30,58 @@ class Seq2SeqGenerator:
             embed = tf.nn.embedding_lookup(embeddings, inputs)
 
         with tf.variable_scope('encode'):
-            lstm_fw_cell = tf.nn.rnn_cell.LSTMCell(num_units=self.enc_units//2, state_is_tuple=True)
+            lstm_fw_cell = tf.nn.rnn_cell.LSTMCell(num_units=self.enc_units//2,
+                                                   state_is_tuple=False, name='lstm_fw')
             lstm_fw_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_fw_cell, output_keep_prob=0.5)
-            lstm_bw_cell = tf.nn.rnn_cell.LSTMCell(num_units=self.enc_units//2, state_is_tuple=True)
+            lstm_bw_cell = tf.nn.rnn_cell.LSTMCell(num_units=self.enc_units//2,
+                                                   state_is_tuple=False, name='lstm_bw')
             lstm_bw_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_bw_cell, output_keep_prob=0.5)
 
-            outputs, states = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, embed, dtype=tf.float32)
+            outputs, states = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, embed,
+                                                              dtype=tf.float32)
 
             # 나온 결과 값을 [batch_size, n_steps, n_hidden] -> [n_steps, batch_size, n_hidden]
-            outputs_fw = tf.transpose(outputs[0], [1, 0, 2])
-            outputs_bw = tf.transpose(outputs[1], [1, 0, 2])
+            # outputs_fw = tf.transpose(outputs[0], [1, 0, 2])
+            # outputs_bw = tf.transpose(outputs[1], [1, 0, 2])
 
             # BLSTM은 나오는 결과 값을 합쳐 준다.
-            outputs_concat = tf.concat([outputs_fw[-1], outputs_bw[-1]], axis=1)
+            outputs_concat = tf.concat(outputs, axis=-1)
+
+            return outputs_concat, states
+
+    def build_decoder(self, inputs):
+        with tf.device('/cpu:0'):
+            embeddings = tf.Variable(
+                tf.random_uniform([self.vocab_size, self.embedding_dim], -1.0, 1.0))
+            # 행렬에 트레이닝 데이터를 지정
+            embed = tf.nn.embedding_lookup(embeddings, inputs)
+
+        with tf.variable_scope('encode'):
+            lstm_fw_cell = tf.nn.rnn_cell.LSTMCell(num_units=self.enc_units//2,
+                                                   state_is_tuple=False, name='lstm_fw')
+            lstm_fw_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_fw_cell, output_keep_prob=0.5)
+            lstm_bw_cell = tf.nn.rnn_cell.LSTMCell(num_units=self.enc_units//2,
+                                                   state_is_tuple=False, name='lstm_bw')
+            lstm_bw_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_bw_cell, output_keep_prob=0.5)
+
+            outputs, states = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell, lstm_bw_cell, embed,
+                                                              dtype=tf.float32)
+
+            # 나온 결과 값을 [batch_size, n_steps, n_hidden] -> [n_steps, batch_size, n_hidden]
+            # outputs_fw = tf.transpose(outputs[0], [1, 0, 2])
+            # outputs_bw = tf.transpose(outputs[1], [1, 0, 2])
+
+            # BLSTM은 나오는 결과 값을 합쳐 준다.
+            outputs_concat = tf.concat(outputs, axis=-1)
+
+            return outputs_concat, states
+
 
 if __name__ == '__main__':
     model = Seq2SeqGenerator(vocab_size=100,
-                             embedding_dim=32,
-                             enc_units=128,
-                             dec_units=128,
+                             embedding_dim=64,
+                             enc_units=256,
+                             dec_units=256,
                              batch_size=32)
-    inputs = tf.placeholder(tf.int32, shape=[32, 16,])
+    inputs = tf.placeholder(tf.int32, shape=[None, 16,])
     model.build_encoder(inputs)
