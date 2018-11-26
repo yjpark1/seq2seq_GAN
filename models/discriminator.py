433c30ff56e210240a49bb935b2aa32fd80b4100
yjpark1 <dmqm.yjpark@gmail.com>
2018-11-26 18:54:00 +0900

    frame

---
+++ models/discriminator.py
@@ -0,0 +1,5 @@
+"""
+input: short text (integer sequence)
+model: input -> embedding -> RNN -> output
+output: real/fake logit
+"""
---
+++ models/reconstructor.py
@@ -0,0 +1,5 @@
+"""
+input: short text (integer sequence)
+model: input -> embedding -> seq2seq(RNN+attention) -> output
+output: long text (integer sequence)
+"""
